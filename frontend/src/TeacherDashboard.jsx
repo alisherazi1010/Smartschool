acdf7b8652b56d9507835a3e79f418e488f82b59
Ali Sherazi <sheraziali1010@gmail.com>
2026-06-06 04:05:12 +0500

    Front updated

--- frontend/src/TeacherDashboard.jsx
+++ frontend/src/TeacherDashboard.jsx
@@ -6,9 +6,7 @@ function TeacherDashboard() {
 
   return (
     <div className="dashboard-layout">
-<button class="menu-toggle" id="menuToggle">☰ Menu</button>
       <aside className="sidebar">
-        
         <h2>SmartSchool</h2>
 
         <button onClick={() => navigate("/teacher")}>Dashboard</button>
